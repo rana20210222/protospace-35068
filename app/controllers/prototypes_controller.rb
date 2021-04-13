dc3dcc13da489e933eac199152214ba705e28d72
rana20210222 <raranana95@yahoo.co.jp>
2021-04-13 21:51:46 +0900

    アクションとルーティング設定・適切なビューファイルを設置

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -19,6 +19,10 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
