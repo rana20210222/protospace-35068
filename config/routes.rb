ded43a80ec36d176c66f05da0de8e9949e570d34
rana20210222 <raranana95@yahoo.co.jp>
2021-04-15 16:02:00 +0900

    コントローラーを作成し、アクションとルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
   resources :prototypes, only: [:new, :create, :show, :update, :edit, :destroy]
-
+  resources :comments, only: [:create]
 end
