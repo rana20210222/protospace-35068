9490ffc21a774295772950b147841f240c3b6a2a
rana20210222 <raranana95@yahoo.co.jp>
2021-04-22 21:26:36 +0900

    ログインしていないユーザーのページ遷移を制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
-  #before_action :move_to_index, except: [:index,]
   before_action :set_prototype, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show]
 
 
   def index
--- db/migrate/20210408065023_remove_title_from_tweets.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTitleFromTweets < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :prottypes, :title, :string
-  end
-end
