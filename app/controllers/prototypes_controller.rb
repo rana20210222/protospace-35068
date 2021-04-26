ac94d2e247d40e11bf6d16d11d0842391d5cd7aa
rana20210222 <raranana95@yahoo.co.jp>
2021-04-26 20:42:59 +0900

    他のユーザーの編集ページに遷移できない修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, only: [:edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :edit]
 
 
   def index
